6586511d11da559e3dd1bd6e12dbcf6a279061ef
keenan <keenan.adimulia@gmail.com>
2026-06-23 14:48:54 +0800

    bmp388 testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,62 @@
-#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
-#define LED_PIN 48   // pin RGB LED (umumnya 48 di ESP32-S3)
-#define NUMPIXELS 1
+#define DebugSerial Serial0
 
-Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define I2C_SDA 17
+#define I2C_SCL 16
+
+#define BMP3XX_I2C_ADDRESS 0x77
+#define BMP3XX_I2C_ADDRESS_ALT 0x76
+
+#define SEALEVELPRESSURE_HPA 1013.25
+
+Adafruit_BMP3XX bmp;
 
 void setup() {
-  pixels.begin();
+  DebugSerial.begin(115200);
+  delay(1000);
+
+  DebugSerial.println("Adafruit BMP388 / BMP390 test");
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  if (!bmp.begin_I2C(BMP3XX_I2C_ADDRESS) &&
+      !bmp.begin_I2C(BMP3XX_I2C_ADDRESS_ALT)) {
+    DebugSerial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (true) {
+      delay(1000);
+    }
+  }
+
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  DebugSerial.println("BMP3XX sensor found.");
 }
 
 void loop() {
-  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // merah
-  pixels.show();
-  delay(1000);
+  if (!bmp.performReading()) {
+    DebugSerial.println("Failed to perform reading.");
+    delay(2000);
+    return;
+  }
 
-  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // hijau
-  pixels.show();
-  delay(1000);
+  DebugSerial.print("Temperature = ");
+  DebugSerial.print(bmp.temperature);
+  DebugSerial.println(" *C");
 
-  // pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // biru
-  // pixels.show();
-  // delay(1000);
+  DebugSerial.print("Pressure = ");
+  DebugSerial.print(bmp.pressure / 100.0);
+  DebugSerial.println(" hPa");
+
+  DebugSerial.print("Approx. Altitude = ");
+  DebugSerial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  DebugSerial.println(" m");
+
+  DebugSerial.println();
+  delay(50);
 }
